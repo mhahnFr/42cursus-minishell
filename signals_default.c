2904f5a73eadc26b9a70a771784b9eda3470f89c
mhahn <mhahn@student.42heilbronn.de>
2022-01-22 00:10:54 +0100

    Fixed signals when prompting

--- signals_default.c
+++ signals_default.c
@@ -1,10 +1,19 @@
+#include <stdio.h>
+#include <readline/readline.h>
 #include <signal.h>
+#include <unistd.h>
 
 #include "signals.h"
 
 static void	signals_default_handler(int s)
 {
-	(void) s;
+	if (s == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_replace_line("", 1);
+	}
+	rl_on_new_line();
+	rl_redisplay();
 }
 
 void	signals_default(void)
