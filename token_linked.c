dddea35cd513c7c9a3bbdb5f12e3ae8b4e60929e
mhahn <mhahn@student.42heilbronn.de>
2022-01-25 00:47:06 +0100

    Added token list deletion

--- token_linked.c
+++ token_linked.c
@@ -2,6 +2,18 @@
 
 #include "token.h"
 
+void	token_list_delete(struct s_token *self)
+{
+	struct s_token	*tmp;
+
+	while (self != NULL)
+	{
+		tmp = self->next;
+		token_delete(self);
+		self = tmp;
+	}
+}
+
 void	token_append(struct s_token **self, struct s_token *appendix)
 {
 	if (*self == NULL)
