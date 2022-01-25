59d97c6d0bedc1f7a03e7649a0f3f89e9598827b
mhahn <mhahn@student.42heilbronn.de>
2022-01-25 01:35:20 +0100

    Only encolouring real ttys

--- main.c
+++ main.c
@@ -14,6 +14,16 @@
 #include "tokenizer.h"
 #include "utils.h"
 
+static void	print_header_part3(void)
+{
+	if (isatty(1))
+		write(1, "\nMade by \033[1;31mnimichi\033[1;0m (github.com/nimichi) and "
+			"\033[1;32mmhahnFr\033[1;0m (github.com/mhahnFr).\n\n", 99);
+	else
+		write(1, "\nMade by nimichi (github.com/nimichi) and "
+			"mhahnFr (github.com/mhahnFr).\n\n", 73);
+}
+
 static void	print_header_part2(void)
 {
 	write(1, "#####################   ######       /###       "
@@ -36,8 +46,7 @@ static void	print_header_part2(void)
 		"=/   # \\\\  *==*  #  \\\\#  #  \\\\#\n", 80);
 	write(1, "                        #                       "
 		"                               \n", 80);
-	write(1, "\nMade by \033[1;31mnimichi\033[1;0m (github.com/nimichi) and "
-		"\033[1;32mmhahnFr\033[1;0m (github.com/mhahnFr).\n\n", 99);
+	print_header_part3();
 }
 
 static void	print_header(void)
@@ -85,9 +94,8 @@ int	main(void)
 				//parse(line, cmd, envp);
 				tokens = tokenize(line);
 				if (tokens != NULL)
-				{
 					//execute(tokens);
-				}
+				token_list_delete(tokens);
 			}
 		}
 		free(line);
