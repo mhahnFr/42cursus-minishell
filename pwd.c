d1fc9022d62ea3e4e7d7e623270ef262fc73e0a3
mhahn <mhahn@student.42heilbronn.de>
2022-02-01 17:53:29 +0100

    Added error management to the pwd builtin

--- pwd.c
+++ pwd.c
@@ -2,12 +2,19 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#include "libft.h"
+
 #include "pwd.h"
 
 int	builtin_pwd(char **argv)
 {
 	char	*pwd;
 
+	if (*argv != NULL)
+	{
+		ft_putendl("Too many arguments!", 2);
+		return (1);
+	}
 	pwd = NULL;
 	pwd = getcwd(pwd);
 	printf("%s\n", pwd);
