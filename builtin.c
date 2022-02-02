4df66e399fbba9959ac8dcf558abfb8c930f0f36
mhahn <mhahn@student.42heilbronn.de>
2022-02-02 19:51:00 +0100

    Fixed builtin call

--- builtin.c
+++ builtin.c
@@ -1,7 +1,8 @@
 #include "libft.h"
 
-#include "token.h"
 #include "env.h"
+#include "token.h"
+#include "utils.h"
 
 int	builtin_check(t_token *token)
 {
@@ -12,7 +13,11 @@ int	builtin_check(t_token *token)
 
 int	builtin_exec(t_token *token)
 {
+	int	exit_status;
+
+	exit_status = -1;
 	if (ft_strncmp(token->c_args[0], "env", ft_strlen(token->c_args[0])) == 0)
-		return (builtin_env(token->c_args, token->envp));
-	return (-1);
+		exit_status = builtin_env(token->c_args, token->envp);
+	utils_free_double_pointer(&token->c_args);
+	return (exit_status);
 }
