c1c5fdcd1dec47c12f2e982e4de3f35a3af872fd
mhahn <mhahn@student.42heilbronn.de>
2022-02-02 20:44:18 +0100

    Added documentation

--- env.h
+++ env.h
@@ -1,6 +1,9 @@
 #ifndef ENV_H
 # define ENV_H
 
+/*
+ * Displays the environment variables.
+ */
 int	builtin_env(char **argv, char **env);
 
 #endif
--- pwd.h
+++ pwd.h
@@ -1,6 +1,9 @@
 #ifndef PWD_H
 # define PWD_H
 
+/*
+ * Displays the current working directory.
+ */
 int	builtin_pwd(char **argv);
 
 #endif
