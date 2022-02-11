1ca778663e4114f3d8845862d39c908950c6f856
mhahn <mhahn@student.42heilbronn.de>
2022-02-12 00:31:30 +0100

    Fixed unset builtin

--- unset.c
+++ unset.c
@@ -10,27 +10,33 @@ static void	builtin_unset_remove(char *var, char ***env)
 	char	**new_env;
 	char	*del;
 	size_t	i;
+	size_t	j;
 
 	del = NULL;
 	i = 0;
 	while ((*env)[i] != NULL)
 	{
-		if (string_starts_with((*env)[i], var))
+		if (string_starts_with((*env)[i], ft_strjoin(var, "=")))
 			del = (*env)[i];
 		i++;
 	}
 	if (del != NULL)
 	{
-		new_env = malloc(get_env_size((const char **) *env));
+		new_env = malloc(get_env_size((const char **) *env) * sizeof(char *));
 		i = 0;
+		j = 0;
 		while ((*env)[i] != NULL)
 		{
 			if ((*env)[i] != del)
-				new_env[i] = (*env)[i];
+			{
+				new_env[j] = (*env)[i];
+				j++;
+			}
 			i++;
 		}
-		new_env[i] = NULL;
-		utils_free_char_array(*env);
+		new_env[j] = NULL;
+		free(*env);
+		free(del);
 		*env = new_env;
 	}
 }
