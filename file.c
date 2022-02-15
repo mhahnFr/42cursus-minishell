8f276cd0277478b907bf888dfa8660ad40c7855e
MichaelNies <michi.ni@gmx.de>
2022-02-15 21:34:44 +0100

    removed whitspace error om file.c

--- file.c
+++ file.c
@@ -6,7 +6,7 @@
 /*   By: mnies <mnies@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 20:00:36 by mhahn             #+#    #+#             */
-/*   Updated: 2022/02/15 20:07:36 by mnies            ###   ########.fr       */
+/*   Updated: 2022/02/15 21:33:54 by mnies            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 #include "str.h"
 #include "token.h"
+#include "utils.h"
 
 void	file_open(t_token *token)
 {
@@ -23,6 +24,8 @@ void	file_open(t_token *token)
 	char	*filestr;
 
 	token_move_one_char(token);
+	while (utils_is_whitespace(token->str[0]))
+		token_move_one_char(token);
 	filestr = str_copy(token);
 	i = open(filestr, O_RDONLY);
 	if (i == -1)
@@ -41,6 +44,8 @@ void	file_write(t_token *token)
 	char	*filestr;
 
 	token_move_one_char(token);
+	while (utils_is_whitespace(token->str[0]))
+		token_move_one_char(token);
 	filestr = str_copy(token);
 	i = open(filestr, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (i == -1)
@@ -60,6 +65,8 @@ void	file_append(t_token *token)
 
 	token_move_one_char(token);
 	token_move_one_char(token);
+	while (utils_is_whitespace(token->str[0]))
+		token_move_one_char(token);
 	filestr = str_copy(token);
 	i = open(filestr, O_WRONLY | O_CREAT | O_APPEND, 0666);
 	if (i == -1)
@@ -78,6 +85,8 @@ void	file_heredoc(t_token *token)
 
 	token_move_one_char(token);
 	token_move_one_char(token);
+	while (utils_is_whitespace(token->str[0]))
+		token_move_one_char(token);
 	while (token->str[0] == ' ')
 		token_move_one_char(token);
 	while (token->str[0] != '\0' && token->str[0] != ' ' && token->str[0] != '|'
