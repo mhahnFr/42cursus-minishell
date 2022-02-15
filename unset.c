ac3b10961d8f65a8051d02accbc1cf7fe1ad09d5
mhahn <mhahn@student.42heilbronn.de>
2022-02-15 15:37:13 +0100

    Norm fixes to the unset builtin

--- unset.c
+++ unset.c
@@ -5,6 +5,32 @@
 #include "unset.h"
 #include "utils.h"
 
+static char	**remove_var(char *var, char ***env)
+{
+	char	**new_env;
+	size_t	i;
+	size_t	j;
+
+	new_env = malloc(get_env_size((const char **) *env) * sizeof(char *));
+	if (new_env == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while ((*env)[i] != NULL)
+	{
+		if ((*env)[i] != var)
+		{
+			new_env[j] = (*env)[i];
+			j++;
+		}
+		i++;
+	}
+	new_env[j] = NULL;
+	free(*env);
+	free(var);
+	return (new_env);
+}
+
 /*
  * Removes the given variable from the given environment. Allocates a new
  * environment.
@@ -14,7 +40,6 @@ static bool	builtin_unset_remove(char *var, char ***env)
 	char	**new_env;
 	char	*del;
 	size_t	i;
-	size_t	j;
 
 	del = NULL;
 	i = 0;
@@ -27,26 +52,12 @@ static bool	builtin_unset_remove(char *var, char ***env)
 	}
 	if (del != NULL)
 	{
-		new_env = malloc(get_env_size((const char **) *env) * sizeof(char *));
+		new_env = remove_var(del, env);
 		if (new_env == NULL)
 		{
 			ft_putendl_fd("Malloc failed in the unset builtin!", 2);
 			return (false);
 		}
-		i = 0;
-		j = 0;
-		while ((*env)[i] != NULL)
-		{
-			if ((*env)[i] != del)
-			{
-				new_env[j] = (*env)[i];
-				j++;
-			}
-			i++;
-		}
-		new_env[j] = NULL;
-		free(*env);
-		free(del);
 		*env = new_env;
 	}
 	return (true);
