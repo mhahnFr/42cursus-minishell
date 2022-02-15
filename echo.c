111f89c08e05fb20226fa4d7d601416b58922f2f
mhahn <mhahn@student.42heilbronn.de>
2022-02-15 15:20:36 +0100

    Fixed the echo builtin

--- echo.c
+++ echo.c
@@ -1,24 +1,47 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "echo.h"
+#include "utils.h"
 
-int	builtin_echo(char **argv)
+static bool	only_ns(char *string)
 {
 	size_t	i;
 
-	i = 1;
-	if (argv != NULL && argv[1] != NULL && argv[1][0] == '-'
-		&& argv[1][0] == 'n' && argv[1][0] == '\0')
+	i = 0;
+	while (string[i] != '\0')
+	{
+		if (string[i] != 'n')
+			return (false);
 		i++;
+	}
+	return (true);
+}
+
+int	builtin_echo(char **argv)
+{
+	bool	nl;
+	bool	stop;
+	size_t	i;
+
+	nl = true;
+	stop = false;
+	i = 1;
 	while (argv[i] != NULL)
 	{
+		if (string_starts_with(argv[i], "-n") && only_ns(&argv[i][1]) && !stop)
+		{
+			nl = false;
+			i++;
+			continue ;
+		}
+		stop = true;
 		printf("%s", argv[i]);
 		if (argv[i + 1] != NULL)
 			printf(" ");
 		i++;
 	}
-	if (!(argv != NULL && argv[1] != NULL && argv[1][0] == '-'
-		&& argv[1][0] == 'n' && argv[1][0] == '\0'))
+	if (nl)
 		printf("\n");
 	return (0);
 }
